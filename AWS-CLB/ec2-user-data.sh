d5f74ccacc3f22a44b53811167d79dae8dcd0631
Hamza Sajjad <hamza37811@gmail.com>
2022-04-21 06:00:26 +0300

    added new folder AWS-CLB

---
+++ AWS-CLB/ec2-user-data.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo 'hello World! from $(hostname -f)' > /var/www/html/index.html
