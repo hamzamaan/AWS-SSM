aad03f88d45d795787c2ed06d0f32ea837b8462c
Hamza Sajjad <hamza37811@gmail.com>
2022-04-21 06:16:33 +0300

    updated

--- AWS-CLB/ec2-user-data.sh
+++ AWS-CLB/ec2-user-data.sh
@@ -4,4 +4,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo 'hello World! from $(hostname -f)' > /var/www/html/index.html
+echo "hello World! from $(hostname -f)" > /var/www/html/index.html
